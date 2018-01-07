8211d59acb233176cbdd3f96ffcdce4090627de8
bimalshah <bimalshah@hotmail.co.uk>
2018-01-07 01:25:30 +0000

    add get data function

--- lib/db.py
+++ lib/db.py
@@ -2,21 +2,21 @@
 import sqlite3
 
 # List of banks that get loaded. TODO Can look to get the list from online
-BANKS = ["Abbey","American Express","Barclays","Citigroup","Lloyds TSB","HBOS","Bank of Ireland Group",
-         "HSBC","NatWest","RBOS","Standard Chartered Bank","Alliance & Leicester","Bradford & Bingley",
-         "Northern Rock","The Woolwich","Adam & Company","Airdrie Savings Bank","Arbuthnot Latham & Co",
-         "Butterfield Private Bank","Cater Allen Private Bank","C.Hoare & Co","Clydesdale Bank ","Co-operative Bank",
-         "Coutts & Co","Drummonds","DB(UK)Bank","Egg","ICICI Bank UK","Icesave","ING Direct","Julian Hodge Bank",
-         "Kleinwort Benson Private Bank Ltd","Raphaels Bank","First Direct","Monzo","Nutmeg","Reliance Bank","Yorkshire Bank","Sainsbury's Bank",
-         "Whiteaway Laidlaw Bank","Nationwide","Britannia","Yorkshire","Coventry","Chelsea","Skipton","Leeds",
-         "West Bromwich","Derbyshire","Principality","Cheshire","Newcastle","Norwich & Peterborough","Dunfermline",
-         "Stroud & Swindon","Nottingham","Scarborough","Kent Reliance","Progressive","Cumberland","National Counties",
-         "Furness","Cambridge","Leek United","Manchester","Saffron","Hinckley & Rugby","Darlington","Newbury",
-         "Monmouthshire","Melton Mowbray","Market Harborough","Ipswich","Barnsley","Marsden","Tipton & Coseley",
-         "Hanley Economic","Mansfield","Teachers'","Loughborough","Chesham","Dudley","Vernon","Scottish",
-         "Bath Investment &","Chorley & District","Harpenden","Holmesdale","Stafford Railway","Beverley",
-         "Buckinghamshire","Swansea","Earl Shilton","Shepshed","Penrith","Ecology","Catholic","City of Derry",
-         "Century Building Society"]
+BANKS = ["Abbey", "American Express", "Barclays", "Citigroup", "Lloyds TSB", "HBOS", "Bank of Ireland Group", "HSBC",
+         "NatWest", "RBOS", "Standard Chartered Bank", "Alliance & Leicester", "Bradford & Bingley", "Northern Rock",
+         "The Woolwich", "Adam & Company", "Airdrie Savings Bank", "Arbuthnot Latham & Co", "Butterfield Private Bank",
+         "Cater Allen Private Bank", "C.Hoare & Co", "Clydesdale Bank ", "Co-operative Bank", "Coutts & Co",
+         "Drummonds", "DB(UK)Bank", "Egg", "ICICI Bank UK", "Icesave", "ING Direct", "Julian Hodge Bank",
+         "Kleinwort Benson Private Bank Ltd", "Raphaels Bank", "First Direct", "Monzo", "Nutmeg", "Reliance Bank",
+         "Yorkshire Bank", "Sainsbury's Bank", "Whiteaway Laidlaw Bank", "Nationwide", "Britannia", "Yorkshire",
+         "Coventry", "Chelsea", "Skipton", "Leeds", "West Bromwich", "Derbyshire", "Principality", "Cheshire",
+         "Newcastle", "Norwich & Peterborough", "Dunfermline", "Stroud & Swindon", "Nottingham", "Scarborough",
+         "Kent Reliance", "Progressive", "Cumberland", "National Counties", "Furness", "Cambridge", "Leek United",
+         "Manchester", "Saffron", "Hinckley & Rugby", "Darlington", "Newbury", "Monmouthshire", "Melton Mowbray",
+         "Market Harborough", "Ipswich", "Barnsley", "Marsden", "Tipton & Coseley", "Hanley Economic", "Mansfield",
+         "Teachers'", "Loughborough", "Chesham", "Dudley", "Vernon", "Scottish", "Bath Investment &",
+         "Chorley & District", "Harpenden", "Holmesdale", "Stafford Railway", "Beverley", "Buckinghamshire", "Swansea",
+         "Earl Shilton", "Shepshed", "Penrith", "Ecology", "Catholic", "City of Derry", "Century Building Society"]
 
 
 def create_new_database(connection):
@@ -41,14 +41,14 @@ def create_new_database(connection):
 
         # todo Need to create transaction table before link and look at the primary key for it
         # The repeat column will be defaulted to 0 and for any duplicate transactions will look at increasing the value
-        sqlcom5 = """CREATE TABLE transactions(id INTEGER NOT NULL PRIMARY KEY, DATE TIMESTAMP, account text NOT NULL, value integer NOT NULL,
-                repeat integer NOT NULL, description text, unique(date, account, value)) """
+        sqlcom5 = """CREATE TABLE transactions(id INTEGER NOT NULL PRIMARY KEY, DATE TIMESTAMP, account text NOT NULL, 
+                    value integer NOT NULL, repeat integer NOT NULL, description text, unique(date, account, value)) """
         cursor.execute(sqlcom5)
         sqlcom6 = """CREATE TABLE category(id INTEGER PRIMARY KEY , category TEXT);
                 CREATE TABLE ml_category(id INTEGER PRIMARY KEY , category TEXT);
-                CREATE TABLE transaction_category(trans_id INTEGER NOT NULL, category_id INTEGER NOT NULL, PRIMARY KEY (trans_id
-                , category_id), FOREIGN KEY(trans_id) REFERENCES transactions(id), FOREIGN KEY(category_id) REFERENCES 
-                category(id));"""
+                CREATE TABLE transaction_category(trans_id INTEGER NOT NULL, category_id INTEGER NOT NULL,
+                PRIMARY KEY (trans_id, category_id), FOREIGN KEY(trans_id) REFERENCES transactions(id),
+                FOREIGN KEY(category_id) REFERENCES category(id));"""
         cursor.executescript(sqlcom6)
         print('Database Created')
 
@@ -112,13 +112,13 @@ def add_transaction(connection, date, account, value, repeat=0, description=None
 def clear_database(connection):
     try:
         cursor = connection.cursor()
-    # Empties the database and then re-initialises it with the create_new_database command
+        # Empties the database and then re-initialises it with the create_new_database command
         cursor.execute("""SELECT name FROM sqlite_master WHERE type='table'""")
         for item in cursor.fetchall():
             cursor.execute("""DROP table IF EXISTS ?""", item)
         print("All transactions deleted")
         # Repopulate database with defaults
-        create_new_database()
+        create_new_database(connection)
     except Exception as e:
         print("Error deleting data from database")
         print(e)
@@ -126,3 +126,15 @@ def clear_database(connection):
     else:
         connection.commit()
 
+
+def get_data(connection, query):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+    except Exception as e:
+        print("Something went wrong, no data for you")
+        print(e)
+        return None
+
+# Function that can handle the filtering when getting data, or handle that in the front side, when connected to the gui
